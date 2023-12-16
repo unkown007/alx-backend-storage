c574ddeaeb6607ccad58bdacc399d2f693c6d0ed
unkown007 <victormatusse00@gmail.com>
2023-12-16 22:37:45 +0200

    added file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table if not exists
+CREATE TABLE IF NOT EXISTS users(
+        id INTEGER AUTO_INCREMENT NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        PRIMARY KEY (id)
+)
