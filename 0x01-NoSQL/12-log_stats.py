1d98fe761f75e31cf506cc4979f04ca35583e82b
unkown007 <victormatusse00@gmail.com>
2023-12-19 19:33:56 +0000

    added file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,17 +3,17 @@
 from pymongo import MongoClient
 
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+if __name__ == "__main__":
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs = client.logs.nginx
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
 
-
-total = logs.find().count()
-print(f"{total} logs")
-print("Methods:")
-for method in methods:
-    aux = logs.find({"method": method}).count()
-    print(f"\tmethod {method}: {aux}")
-status = logs.find({"path": "/status"}).count()
-print(f"{status} status check")
+    total = logs.find().count()
+    print(f"{total} logs")
+    print("Methods:")
+    for method in methods:
+        aux = logs.find({"method": method}).count()
+        print(f"\tmethod {method}: {aux}")
+    status = logs.find({"path": "/status"}).count()
+    print(f"{status} status check")
