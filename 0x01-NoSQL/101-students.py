25f5e0930563727b564f083e484d3db768451663
unkown007 <victormatusse00@gmail.com>
2023-12-19 20:13:20 +0000

    added file

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Define top_students """
+
+
+def top_students(mongo_collection):
+    """ Return all students sorted by average score """
+    students = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return students
