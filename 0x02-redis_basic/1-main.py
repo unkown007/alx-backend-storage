a8da4badb3f24f2aa05f28237fb31410fea70f77
unkown007 <victormatusse00@gmail.com>
2024-05-06 13:38:18 +0200

    added file

--- 0x02-redis_basic/1-main.py
+++ 0x02-redis_basic/1-main.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
-
-Cache = __import__('exercise').Cache
-
-cache = Cache()
-
-data = b"hello"
-key = cache.store(data)
-print(key)
-
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
 cache = Cache()
 
 TEST_CASES = {
--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module define and implements Cache class
 """
-from typing import Union
+from typing import Union, Callable
 from uuid import uuid4
 import redis
 
@@ -19,3 +19,17 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key, fn=None):
+        value = self._redis.get(key)
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key):
+        """ Get a value in Redis db """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key):
+        """ Get a value in Redis db """
+        return self.get(key, int)
